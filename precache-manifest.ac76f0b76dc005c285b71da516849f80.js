5303d45c15d8be1937500b4cc794a9ece72c3580
restrepobs <brian@viulogix.com>
2020-06-21 16:03:49 -0400

    Updates

---
+++ precache-manifest.ac76f0b76dc005c285b71da516849f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dc7ab777bbc0c870f96d32809e27b3",
+    "url": "/restrepobs.github.io-COVID19/index.html"
+  },
+  {
+    "revision": "965f5d4f74196afb0758",
+    "url": "/restrepobs.github.io-COVID19/static/css/main.c5e02e06.chunk.css"
+  },
+  {
+    "revision": "747a9edc9fdb7bbe37a5",
+    "url": "/restrepobs.github.io-COVID19/static/js/2.c2322daf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/restrepobs.github.io-COVID19/static/js/2.c2322daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965f5d4f74196afb0758",
+    "url": "/restrepobs.github.io-COVID19/static/js/main.44061950.chunk.js"
+  },
+  {
+    "revision": "7f1085b7e8e3fe6d9d80",
+    "url": "/restrepobs.github.io-COVID19/static/js/runtime-main.59fb4a4b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/restrepobs.github.io-COVID19/static/media/covid.d7265326.png"
+  }
+]);
